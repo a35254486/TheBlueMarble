6c41423b1f36eec0937484996336839561dd867b
a35254486 <a35254486@gmail.com>
2026-02-13 11:47:23 -0500

    Update helloWorldWind.js

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -2,9 +2,6 @@
 var wwd = new WorldWind.WorldWindow("canvasOne");
 
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
-
-/*wwd.addLayer(new "images/PathfinderMap.jpg"));*/
-
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 
 wwd.addLayer(new WorldWind.CompassLayer());
@@ -12,10 +9,10 @@ wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
 
 // Add a placemark
-
 /*
 var placemarkLayer = new WorldWind.RenderableLayer();
 wwd.addLayer(placemarkLayer);
+
 var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
 
 placemarkAttributes.imageOffset = new WorldWind.Offset(
@@ -26,8 +23,7 @@ placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
     WorldWind.OFFSET_FRACTION, 0.5,
     WorldWind.OFFSET_FRACTION, 1.0);
 
-placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
-*/
+placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";*/
 
 var position = new WorldWind.Position(55.0, -106.0, 100.0);
 var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
