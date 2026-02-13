7e53073bfb2c4a8b9bdf62d2f866c3777f74e3dd
a35254486 <a35254486@gmail.com>
2026-02-13 12:00:36 -0500

    Update helloWorldWind.js

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -1,74 +1,27 @@
-/*// Create a WorldWindow for the canvas.
-var wwd = new WorldWind.WorldWindow("canvasOne");
-
-wwd.addLayer(new WorldWind.BMNGOneImageLayer());
-wwd.addLayer(new WorldWind.BMNGLandsatLayer());
-
-wwd.addLayer(new WorldWind.CompassLayer());
-wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
-*/
-
 // Create a WorldWindow for the canvas.
 var wwd = new WorldWind.WorldWindow("canvasOne");
 
-// OLD, PIXELATED LAYER IS REMOVED:
-// wwd.addLayer(new WorldWind.BMNGLandsatLayer());
-
-// NEW, HIGH-QUALITY LAYER IS ADDED:
-// It will look in your 'images' folder for a file named 'PathfinderMap.jpg'
+// ADD THE HIGH-QUALITY LOCAL IMAGE LAYER.
+// This works now because the full library was loaded first in index.html.
 wwd.addLayer(new WorldWind.SurfaceImageLayer(WorldWind.Sector.FULL_SPHERE, "images/PathfinderMap.jpg"));
 
-// These other layers for controls are good. Keep them.
+// Add other essential layers
 wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
-
-// ... the rest of your polygon and model code remains the same ...
-
-
-// Add a placemark
-/*
-var placemarkLayer = new WorldWind.RenderableLayer();
-wwd.addLayer(placemarkLayer);
-
-var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-
-placemarkAttributes.imageOffset = new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.3,
-    WorldWind.OFFSET_FRACTION, 0.0);
-
-placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.5,
-    WorldWind.OFFSET_FRACTION, 1.0);
-
-placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";*/
-
-var position = new WorldWind.Position(55.0, -106.0, 100.0);
-var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
-
-placemark.label = "Placemark\n" +
-    "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
-    "Lon " + placemark.position.longitude.toPrecision(5).toString();
-placemark.alwaysOnTop = true;
-
-placemarkLayer.addRenderable(placemark);
 
 // Add a polygon
 var polygonLayer = new WorldWind.RenderableLayer();
 wwd.addLayer(polygonLayer);
-
 var polygonAttributes = new WorldWind.ShapeAttributes(null);
 polygonAttributes.interiorColor = new WorldWind.Color(0, 1, 1, 0.75);
 polygonAttributes.outlineColor = WorldWind.Color.BLUE;
 polygonAttributes.drawOutline = true;
 polygonAttributes.applyLighting = true;
-
 var boundaries = [];
 boundaries.push(new WorldWind.Position(20.0, -75.0, 700000.0));
 boundaries.push(new WorldWind.Position(25.0, -85.0, 700000.0));
 boundaries.push(new WorldWind.Position(20.0, -95.0, 700000.0));
-
 var polygon = new WorldWind.Polygon(boundaries, polygonAttributes);
 polygon.extrude = true;
 polygonLayer.addRenderable(polygon);
@@ -76,10 +29,8 @@ polygonLayer.addRenderable(polygon);
 // Add a COLLADA model
 var modelLayer = new WorldWind.RenderableLayer();
 wwd.addLayer(modelLayer);
-
 var position = new WorldWind.Position(10.0, -125.0, 800000.0);
 var config = {dirPath: WorldWind.configuration.baseUrl + 'examples/collada_models/duck/'};
-
 var colladaLoader = new WorldWind.ColladaLoader(position, config);
 colladaLoader.load("duck.dae", function (colladaModel) {
     colladaModel.scale = 9000;
@@ -89,7 +40,6 @@ colladaLoader.load("duck.dae", function (colladaModel) {
 // Add WMS imagery
 var serviceAddress = "https://neo.gsfc.nasa.gov/wms/wms?version=1.3.0&service=WMS&request=GetCapabilities";
 var layerName = "MOD_LSTD_CLIM_M";
-
 var createLayer = function (xmlDom) {
     var wms = new WorldWind.WmsCapabilities(xmlDom);
     var wmsLayerCapabilities = wms.getNamedLayer(layerName);
@@ -97,11 +47,7 @@ var createLayer = function (xmlDom) {
     var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
     wwd.addLayer(wmsLayer);
 };
-
 var logError = function (jqXhr, text, exception) {
-    console.log("There was a failure retrieving the capabilities document: " +
-        text +
-    " exception: " + exception);
+    console.log("There was a failure retrieving the capabilities document: " + text + " exception: " + exception);
 };
-
 $.get(serviceAddress).done(createLayer).fail(logError);
